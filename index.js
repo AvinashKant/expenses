ac4b9d1df43acae2b8153d86516f694bd93ae9af
Avinash Kant <5821008-AvinashKant@users.noreply.gitlab.com>
2024-06-09 00:09:31 +0530

    cros

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 dotenv.config({ path: './.env' });
 const app = express();
 const categories = require("./routes/category");
@@ -8,7 +10,7 @@ const dailyExpenses = require("./routes/dailyexpenses");
 
 const dbConfig = require('./config/database.config.js');
 
-
+app.use(cors({ origin: '*' }));
 
 
 // Connect to MongoDB database
